bde8b084a9dad36ac7d6b2628b6c91feb01e9120
gauravghumare144-afk <gauravghumare144@gmail.com>
2026-08-15 16:25:50 +0530

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE="pria-v2";
-const ASSETS=["./","./index.html","./manifest.json","./icons/icon-192.png","./icons/icon-512.png"];
-self.addEventListener("install",e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener("activate",e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(res=>{const copy=res.clone();caches.open(CACHE).then(c=>c.put(e.request,copy));return res}).catch(()=>caches.match("./index.html"))));
+const C="pria-v6";const F=["./","./index.html","./style.css","./app.js","./manifest.json","./icon.svg","./sir-photo.png"];
+self.addEventListener("install",e=>e.waitUntil(caches.open(C).then(c=>c.addAll(F))));
+self.addEventListener("activate",e=>e.waitUntil(self.clients.claim()));
+self.addEventListener("fetch",e=>e.respondWith(caches.match(e.request).then(x=>x||fetch(e.request))));
